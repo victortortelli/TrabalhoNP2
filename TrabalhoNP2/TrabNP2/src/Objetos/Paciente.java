6d31d1116d946cb2cab8e25a501a6a74e6be442a
Guilherme Pupo Cardoso <guilh.pupo@gmail.com>
2019-11-02 21:49:49 -0300

    terminada PacienteDAO falta fazer os testes de consistencia dentro de cadastrogui

--- TrabalhoNP2/TrabNP2/src/Objetos/Paciente.java
+++ TrabalhoNP2/TrabNP2/src/Objetos/Paciente.java
@@ -10,6 +10,125 @@ import java.util.ArrayList;
  * @author guilherme
  */
 public class Paciente extends Pessoas{
+
+    /**
+     * @return the cartaoSUS
+     */
+    public String getCartaoSUS() {
+        return cartaoSUS;
+    }
+
+    /**
+     * @param cartaoSUS the cartaoSUS to set
+     */
+    public void setCartaoSUS(String cartaoSUS) {
+        this.cartaoSUS = cartaoSUS;
+    }
+
+    /**
+     * @return the endereco
+     */
+    public String getEndereco() {
+        return endereco;
+    }
+
+    /**
+     * @param endereco the endereco to set
+     */
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    /**
+     * @return the bairro
+     */
+    public String getBairro() {
+        return bairro;
+    }
+
+    /**
+     * @param bairro the bairro to set
+     */
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    /**
+     * @return the numero
+     */
+    public String getNumero() {
+        return numero;
+    }
+
+    /**
+     * @param numero the numero to set
+     */
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    /**
+     * @return the complemento
+     */
+    public String getComplemento() {
+        return complemento;
+    }
+
+    /**
+     * @param complemento the complemento to set
+     */
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    /**
+     * @return the cidade
+     */
+    public String getCidade() {
+        return cidade;
+    }
+
+    /**
+     * @param cidade the cidade to set
+     */
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    /**
+     * @return the estado
+     */
+    public String getEstado() {
+        return estado;
+    }
+
+    /**
+     * @param estado the estado to set
+     */
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    /**
+     * @return the escola
+     */
+    public String getEscola() {
+        return escola;
+    }
+
+    /**
+     * @param escola the escola to set
+     */
+    public void setEscola(String escola) {
+        this.escola = escola;
+    }
+
+    /**
+     * @return the consultas
+     */
+    public ArrayList<Consulta> getConsultas() {
+        return consultas;
+    }
     private String cartaoSUS;
     private String endereco;
     private String bairro;
@@ -17,10 +136,40 @@ public class Paciente extends Pessoas{
     private String complemento;
     private String cidade;
     private String estado;
+    private String ddd;
+    private String telefone;
     private String escola;
     private ArrayList<Consulta> consultas;
     
     public void listarConsultas(){
         //fazer dps
+    }
+
+    /**
+     * @return the ddd
+     */
+    public String getDdd() {
+        return ddd;
+    }
+
+    /**
+     * @param ddd the ddd to set
+     */
+    public void setDdd(String ddd) {
+        this.ddd = ddd;
+    }
+
+    /**
+     * @return the telefone
+     */
+    public String getTelefone() {
+        return telefone;
+    }
+
+    /**
+     * @param telefone the telefone to set
+     */
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
     }
 }
